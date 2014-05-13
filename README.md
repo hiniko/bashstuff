59ae62e41316dbe7ad099c5536c01937ff46f4af
Paul Rose <paul@paulrose.me>
2014-05-13 11:03:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# BashStuff
+__A collection of helpul BASH functions to make writing and using scripts
+a Joy__
+
---
+++ bashstuff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# BashStuff 
+# A collection of helpful BASH functions to make writing and using scripts
+#
+# Author: Paul Rose
+# Email: projects@paulrose.me
+# Repo: https://github.com/niko-niko/bashstuff
